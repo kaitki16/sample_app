cee6edc8d61c08a09fc3cc7722b5157da07386b5
EC2 Default User <ec2-user@ip-172-31-39-95.us-east-2.compute.internal>
2020-01-14 06:09:04 +0000

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,6 +63,7 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+  
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = ''*6
     assert_not @user.valid?
